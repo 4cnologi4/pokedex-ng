b58661b22b00258a4e0a7c8a6237d5cb8427f216
4cnologi4 <daniell9404@outlook.com>
2020-11-10 17:26:19 -0600

    Material Icons

--- src/app/material-module/angular-material.module.ts
+++ src/app/material-module/angular-material.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  imports: [MatCardModule, MatButtonModule],
-  exports: [MatCardModule, MatButtonModule]
+  exports: [MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule]
 })
 export class AngularMaterialModule {}

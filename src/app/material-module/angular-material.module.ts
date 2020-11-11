e95ba647325d5ddde862d56a75f7093abd717e56
4cnologi4 <daniell9404@outlook.com>
2020-11-11 13:01:45 -0600

    1 file modified

--- src/app/material-module/angular-material.module.ts
+++ src/app/material-module/angular-material.module.ts
@@ -5,7 +5,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
-import { MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from "@angular/material/menu";
 
 @NgModule({
   exports: [

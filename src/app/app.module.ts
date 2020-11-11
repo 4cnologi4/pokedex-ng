f4ae28f46c41b366bbb37ff05631c57d331b20f1
4cnologi4 <daniell9404@outlook.com>
2020-11-11 13:57:46 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularMaterialModule } from "./material-module/angular-material.module";
 
 import { AppComponent } from "./app.component";
@@ -14,6 +15,7 @@ import { ContextMenuComponent } from "./components/context-menu/context-menu.com
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AngularMaterialModule
   ],
   declarations: [

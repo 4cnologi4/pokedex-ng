5eef4bf6fc82ed68bf4e76387da3a618c859cfb1
4cnologi4 <daniell9404@outlook.com>
2020-11-11 19:09:40 -0600

    4 files modified

--- src/app/components/context-menu/context-menu.component.ts
+++ src/app/components/context-menu/context-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ViewChild, Output, EventEmitter } from "@angular/core";
 import { MatMenuTrigger } from "@angular/material/menu";
 
 import { Pokemon } from "../../models/pokemon.interface";
@@ -8,11 +8,37 @@ import { Pokemon } from "../../models/pokemon.interface";
   templateUrl: "context-menu.component.html"
 })
 export class ContextMenuComponent {
-  items = [
-    { id: 1, name: "Item 1" },
-    { id: 2, name: "Item 2" },
-    { id: 3, name: "Item 3" }
-  ];
+  // items = [
+  //   { id: 1, name: "Item 1" },
+  //   { id: 2, name: "Item 2" },
+  //   { id: 3, name: "Item 3" }
+  // ];
+
+  // @ViewChild(MatMenuTrigger)
+  // contextMenu: MatMenuTrigger;
+
+  // contextMenuPosition = { x: "0px", y: "0px" };
+
+  // onContextMenu(event: MouseEvent, item: Item) {
+  //   event.preventDefault();
+  //   this.contextMenuPosition.x = event.clientX + "px";
+  //   this.contextMenuPosition.y = event.clientY + "px";
+  //   this.contextMenu.menuData = { item: item };
+  //   this.contextMenu.menu.focusFirstItem("mouse");
+  //   this.contextMenu.openMenu();
+  // }
+
+  // onContextMenuAction1(item: Item) {
+  //   alert(`Click on Action 1 for ${item.name}`);
+  // }
+
+  // onContextMenuAction2(item: Item) {
+  //   alert(`Click on Action 2 for ${item.name}`);
+  // }
+
+  @Output() pokemonMenuEvent = new EventEmitter<Pokemon>();
+
+  items = [{ id: 1, name: "Item 1" }, { id: 2, name: "Item 2" }];
 
   @ViewChild(MatMenuTrigger)
   contextMenu: MatMenuTrigger;
@@ -28,21 +54,14 @@ export class ContextMenuComponent {
     this.contextMenu.openMenu();
   }
 
-  onContextMenuAction1(item: Item) {
-    alert(`Click on Action 1 for ${item.name}`);
+  onContextMenuAction1(pokemon: Pokemon) {
+    // alert(`Click on Action 1 for ${pokemon.name}`);
+    this.pokemonMenuEvent.emit(pokemon);
   }
 
-  onContextMenuAction2(item: Item) {
-    alert(`Click on Action 2 for ${item.name}`);
+  onContextMenuAction2(pokemon: Pokemon) {
+    alert(`Click on Action 2 for ${pokemon.name}`);
   }
-
-  // onContextMenuAction1(pokemon: Pokemon) {
-  //   alert(`Click on Action 1 for ${pokemon.name}`);
-  // }
-
-  // onContextMenuAction2(pokemon: Pokemon) {
-  //   alert(`Click on Action 2 for ${pokemon.name}`);
-  // }
 }
 
 export interface Item {
--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild } from "@angular/core";
 
 import { Pokemon } from "../../models/pokemon.interface";
 
 import { PokemonService } from "../../services/pokemon.service";
+
+import { ContextMenuComponent } from "../context-menu/context-menu.component";
 
 @Component({
   selector: "pokemon-component",
@@ -10,12 +12,18 @@ import { PokemonService } from "../../services/pokemon.service";
   styleUrls: ["./pokemon.component.css"]
 })
 export class PokemonComponent implements OnInit {
+  @ViewChild(ContextMenuComponent, {static: false}) contextMenu: ContextMenuComponent;
   public pokemon: Pokemon;
   @Input() url: string;
   public front: boolean;
   public urlImage: string;
 
   constructor(private _pokeService: PokemonService) {}
+
+  openMenu(event: MouseEvent, item: Item) {
+    console.log(this.contextMenu);
+    this.contextMenu.onContextMenu(event, item);
+  }
 
   ngOnInit() {
     this.getPokemon(this.url);
@@ -35,4 +43,9 @@ export class PokemonComponent implements OnInit {
       : this.pokemon.sprites.back_default;
     this.front = !this.front;
   }
+}
+
+export interface Item {
+  id: number;
+  name: string;
 }
